c2d8d4350f6aeb61f9a2b143c790897d7b74b6f2
Stefan Zeiger <szeiger@novocode.com>
2013-11-08 17:50:18 +0100

    Scala 2.11 compatibility.
    - Convert \0 to \u0000
    - Change TupleClass handling in SlickBackend
    - add explicit bounds on existentials: bound inference via
      SI-1786 is compilation order dependent!
    - comment out part of a test that currently gives a type
      error.

--- slick-testkit/src/test/scala/scala/slick/test/direct/QueryableTest.scala
+++ slick-testkit/src/test/scala/scala/slick/test/direct/QueryableTest.scala
@@ -90,7 +90,7 @@ class QueryableTest(val tdb: JdbcTestDB) extends DBTest {
     def assertNoMatch[T:TypeTag:ClassTag]( queryable:Queryable[T], expected: Traversable[T] ) = try{
       assertEqualMultiSet( backend.result(queryable,dynamicSession), expected)
     } catch {
-      case e:AssertionError => 
+      case e:AssertionError =>
       case e:Throwable => throw e
     }
     def assertMatchOrdered[T:TypeTag:ClassTag]( queryable:Queryable[T], expected: Traversable[T] ) = assertEquals( expected, backend.result(queryable,dynamicSession) )
@@ -101,12 +101,12 @@ class QueryableTest(val tdb: JdbcTestDB) extends DBTest {
     val q1 = qoo.map( _.sales + 5 )
   }
   def initialStringOptionOrdering = implicitly[Ordering[Option[String]]]
-  
+
   @Test def test() {
     implicit var stringOptionOrdering : scala.math.Ordering[Option[String]] = initialStringOptionOrdering
 
     import TestingTools._
-    
+
     val coffees_data = Vector(
       ("Colombian",          1, None),
       ("French_Roast",       2, None),
@@ -117,7 +117,7 @@ class QueryableTest(val tdb: JdbcTestDB) extends DBTest {
       ("Colombian_Decaf",    3, Some("White Chocolate")),
       ("French_Roast_Decaf", 5, None)
     )
-    
+
     db withDynSession {
       // create test table
       sqlu"create table COFFEES(COF_NAME varchar(255), SALES int, FLAVOR varchar(255) NULL)".execute
@@ -126,7 +126,7 @@ class QueryableTest(val tdb: JdbcTestDB) extends DBTest {
       } yield sqlu"insert into COFFEES values ($name, $sales, $flavor)".first).sum
 
       // FIXME: reflective typecheck failed:  backend.result(Queryable[Coffee].map(x=>x))
-      
+
       // setup query and equivalent inMemory data structure
       val inMem = coffees_data.map{ case (name, sales,flavor) => Coffee(name, sales,flavor) }
       val query : Queryable[Coffee] = Queryable[Coffee]
@@ -141,22 +141,22 @@ class QueryableTest(val tdb: JdbcTestDB) extends DBTest {
       // FIXME: make this test less artificial
       class MyQuerycollection{
         def findUserByName( name:String ) = query.filter( _.name == name )
-      }  
+      }
       val qc = new MyQuerycollection
       qc.findUserByName("some value")
-      
+
       // test singleton object
       assertMatch(
         Singleton.q1,
         inMem.map( _.sales + 5 )
       )
-      
+
       // test singleton in package
       assertMatch(
         Singleton.q1,
         inMem.map( _.sales + 5 )
       )
-      
+
       // test singleton in singleton in package
       assertMatch(
         Singleton.Singleton.q1,
@@ -177,7 +177,7 @@ class QueryableTest(val tdb: JdbcTestDB) extends DBTest {
         )
         fail()
       }catch{
-        case _:SlickException => 
+        case _:SlickException =>
       }
 
       // simple map
@@ -185,20 +185,20 @@ class QueryableTest(val tdb: JdbcTestDB) extends DBTest {
         query.map( (_:Coffee).sales + 5 ),
         inMem.map( (_:Coffee).sales + 5 )
       )
-      
+
       // left-hand-side coming from attribute
       val foo = new Foo(query)
       assertMatch(
         foo.q.map( (_:Coffee).sales + 5 ),
         inMem.map( (_:Coffee).sales + 5 )
       )
-  
+
       // map with string concatenation
       assertMatch(
         query.map( _.name + "." ),
         inMem.map( _.name + "." )
       )
-  
+
       // filter with more complex condition
       assertMatch(
         query.filter( c => c.sales > 2 || "Colombian_Decaf" == c.name ),
@@ -221,7 +221,7 @@ class QueryableTest(val tdb: JdbcTestDB) extends DBTest {
         query.map( _.name ).filter(_ == ""),
         inMem.map( _.name ).filter(_ == "")
       )
-  
+
       // referenced values are inlined as constants using reflection
       val o = 2 + 3
       assertMatch(
@@ -229,12 +229,12 @@ class QueryableTest(val tdb: JdbcTestDB) extends DBTest {
         inMem.filter( _.sales > o )
       )
 
-      // nesting (not supported yet: query.map(e1 => query.map(e2=>e1))) 
+      // nesting (not supported yet: query.map(e1 => query.map(e2=>e1)))
       assertMatch(
         query.flatMap(e1 => query.map(e2=>e1)),
         inMem.flatMap(e1 => inMem.map(e2=>e1))
       )
-  
+
       // query scope
       {
         val inMemResult = inMem.filter( _.sales > 5 )
@@ -244,7 +244,7 @@ class QueryableTest(val tdb: JdbcTestDB) extends DBTest {
           Queryable{
             val foo = query
             val bar = foo.filter( _.sales > 5 )
-            bar  
+            bar
           }
         ).foreach{
           query_ => assertMatch( query_, inMemResult )
@@ -256,7 +256,7 @@ class QueryableTest(val tdb: JdbcTestDB) extends DBTest {
         for( c <- query ) yield c.name,
         for( c <- inMem ) yield c.name
       )
-  
+
       // nesting with flatMap
       {
         val inMemResult = for( o <- inMem; i <- inMem ) yield i.name
@@ -272,7 +272,7 @@ class QueryableTest(val tdb: JdbcTestDB) extends DBTest {
       assertMatch(
         query.flatMap(e1 => query.map(e2=>e1).map(e2=>e1)),
         inMem.flatMap(e1 => inMem.map(e2=>e1).map(e2=>e1))
-      ) 
+      )
 
       // nesting with outer macro reference
       {
@@ -285,7 +285,7 @@ class QueryableTest(val tdb: JdbcTestDB) extends DBTest {
           query_ => assertMatch( query_, inMemResult )
         }
       }
-  
+
       // nesting with chaining / comprehension with cartesian product and if
       {
         val inMemResult = for( o <- inMem; i <- inMem; if i.sales == o.sales ) yield i.name
@@ -303,7 +303,7 @@ class QueryableTest(val tdb: JdbcTestDB) extends DBTest {
         query.map( c=> (c.name,c.sales) ),
         inMem.map( c=> (c.name,c.sales) )
       )
-      
+
       // nested structures (here tuples and case classes)
       assertMatch(
         query.map( c=> (c.name,c.sales,c) ),
@@ -314,67 +314,67 @@ class QueryableTest(val tdb: JdbcTestDB) extends DBTest {
 
       val iquery = ImplicitQueryable( query, backend, dynamicSession )
       assertEquals( iquery.length, inMem.length )
-      
+
       // iquery.filter( _.sales > 10.0 ).map( _.name ) // currently crashed compiler
-      
+
       assertMatch(
         query.map( c=>c ),
         iquery.map( c=>c ).toSeq
       )
-      
+
       ({
         import ImplicitQueryable.implicitExecution._
         assertMatch(
           query.map( c=>c ),
-          iquery.map( c=>c )
+          null //iquery.map( c=>c ) // TODO 2.11 why doesn't this typecheck anymore? Broken since (at least) 2.11.0-M8
         )
       })
-   
+
       assertMatch(
            for( o <-  query; i <-  query; if i.sales == o.sales  ) yield i.name,
           (for( o <- iquery; i <- iquery; if i.sales == o.sales ) yield i.name).toSeq
       )
-      
+
       assertMatch(
         for( v1<-query;v2<-query; if !(v1.name == v2.name)) yield (v1.name,v2.name)
         ,for( v1<-inMem;v2<-inMem; if !(v1.name == v2.name)) yield (v1.name,v2.name)
       )
-      
+
       assertMatch(
         for( v1<-query;v2<-query; if v1.name != v2.name) yield (v1.name,v2.name)
         ,for( v1<-inMem;v2<-inMem; if v1.name != v2.name) yield (v1.name,v2.name)
       )
-      
+
       assertMatch(
         query.take(2)
         ,inMem.take(2)
       )
-      
+
       assertMatch(
         query.drop(2)
         ,inMem.drop(2)
       )
-      
+
       assertMatchOrdered(
         query.sortBy(_.name)
         ,inMem.sortBy(_.name)
       )
-      
+
       assertMatchOrdered(
         query.sortBy(c=>(c.name,c.flavor))
         ,inMem.sortBy(c=>(c.name,c.flavor))
       )
-      
+
       assertMatchOrdered(
         query.sortBy(c => reversed(c.name))
         ,inMem.sortBy(c => reversed(c.name))
-      )      
-      
+      )
+
       assertMatchOrdered(
         query.sortBy(c => (c.name,reversed(c.sales)))
         ,inMem.sortBy(c => (c.name,reversed(c.sales)))
       )
-      
+
       def nullOrdering( x:Int, y:Int ) = new scala.math.Ordering[Option[String]]{
         def compare(a:Option[String],b:Option[String]) = {
           if( a == None && b == None ) 0
@@ -407,7 +407,7 @@ class QueryableTest(val tdb: JdbcTestDB) extends DBTest {
         query.sortBy(c=>(nonesFirst(reversed(c.flavor)),c.name))
         ,inMem.sortBy(c=>(c.flavor,c.name))
       )
-      
+
       stringOptionOrdering = initialStringOptionOrdering
       assertMatchOrdered(
         query.sortBy( c => (
